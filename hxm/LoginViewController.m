fffce81e07cb8f2d56ce0e1e75962288391b47c6
Bruce <bruce@nantekimbp.lan>
2015-05-15 01:19:07 +0800

    login page

--- hxm/LoginViewController.m
+++ hxm/LoginViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "LoginViewController.h"
+#import "RegisterViewController.h"
 #import "AFNetworkTool.h"
 #import "BWCommon.h"
 
@@ -28,6 +29,24 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (void) registerTouched:(id)sender
+{
+    
+    RegisterViewController * registerView = [[RegisterViewController alloc] init];
+    
+    [self presentViewController:registerView animated:YES completion:^(void){
+        
+        NSString *username = [BWCommon getUserInfo:@"username"];
+        if(username != nil){
+            //注册成功
+        }
+        NSLog(@"register view closed");
+    }];
+    
+    NSLog(@"register touched");
+}
+
 - (void)pageLayout {
     
     self.view.backgroundColor = [UIColor colorWithRed:240/255.0f green:240/255.0f blue:250/255.0f alpha:1];
@@ -35,8 +54,20 @@
     //logo
     UIImage *logo = [UIImage imageNamed:@"logo.png"];
     UIImageView *ivLogo = [[UIImageView alloc] initWithImage:logo];
+    ivLogo.frame = CGRectMake(50, 100, 220, 100);
+    ivLogo.contentMode = UIViewContentModeCenter;
     
     [self.view addSubview:ivLogo];
+    
+//注册连接
+    
+    UIButton *btnRegister = [[UIButton alloc] initWithFrame:CGRectMake(10, 20, 80, 30)];
+    btnRegister.tintColor = [UIColor grayColor];
+    [btnRegister setTitle:@"注册账号" forState:UIControlStateNormal];
+    [btnRegister addTarget:self action:@selector(registerTouched:)forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:btnRegister];
+
     
 }
 
---
+++ hxm/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  hxm
+//
+//  Created by Bruce on 15-5-14.
+//  Copyright (c) 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
